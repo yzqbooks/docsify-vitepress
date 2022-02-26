046b25f228fe811cc0e5783d5baf63839f928626
yzqdev <yzqdev@outlook.com>
2022-02-26 23:08:18 +0800

    update nav

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -45,10 +45,26 @@ function getGuideSidebar() {
         { text: "配置项", link: "/configuration" },
         { text: "主题", link: "/themes" },
         { text: "使用插件", link: "/plugins" },
+        { text: "开发插件", link: "/write-a-plugin" },
+
         {
           text: "markdown配置",
           link: "/markdown",
         },
+        { text: "代码高亮", link: "/language-highlight" },
+      ],
+    },
+    {
+      text: "指南",
+      children: [
+        { text: "部署", link: "/deploy" },
+        { text: "文档助手", link: "/helpers" },
+        { text: "vue", link: "/vue" },
+        { text: "cdn", link: "/cdn" },
+        { text: "离线模式", link: "/pwa" },
+        { text: "服务端渲染", link: "/ssr" },
+        { text: "文件嵌入", link: "/embed-files" },
+        { text: "awesome", link: "/awesome" },
       ],
     },
   ];
